813bef0d2873a5aede63500d1428e930720b2f4d
Micha Alt <php@lean-stack.de>
2018-08-15 21:27:47 +0200

    Allright, brilliant implementation.

--- spec/MiddlewareSpec.php
+++ spec/MiddlewareSpec.php
@@ -5,11 +5,17 @@ namespace spec\Peesar;
 use Peesar\Middleware;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Http\Server\MiddlewareInterface;
 
 class MiddlewareSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType(Middleware::class);
+    }
+
+    function it_implements_psr_middleware_interface()
+    {
+        $this->shouldImplement(MiddlewareInterface::class);
     }
 }
--- src/Middleware.php
+++ src/Middleware.php
@@ -2,6 +2,19 @@
 
 namespace Peesar;
 
-class Middleware
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class Middleware implements MiddlewareInterface
 {
+    /**
+     * Process an incoming server request and return a response, optionally delegating
+     * response creation to a handler.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $handler->handle($request);
+    }
 }
